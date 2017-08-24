537e51b4d868fc32c56efd923a4691ab1dc96d13
Trung-Hieu Dam <hieu_fire@hotmail.com>
2017-08-23 19:22:32 -0700

    Updated BasicsApp

--- src/main/java/com/sqa/td/BasicApp.java
+++ src/main/java/com/sqa/td/BasicApp.java
@@ -1,18 +1,47 @@
-/**
- * File Name: BasicApp.java<br>
- * Trung-Hieu Dam<br>
- * Java Boot Camp Exercise<br>
- * Instructor: Jean-francois Nepton<br>
- * Created: Aug 12, 2017
- */
 package com.sqa.td;
+
+import com.sqa.td.helpers.*;
 
 public class BasicApp {
 
 	public static void main(String[] args) {
-		System.out.print("┏(-_-)┛");
-		System.out.print("┗(-_- )┓");
-		System.out.print("┗(-_-)┛");
-		System.out.print("┏(-_-)┓");
+		String username = AppBasics.greetUserAndGetName("Default App Exmple");
+		checkAgainst10();
+		checkCurMonth();
+		checkGradeValue();
+		AppBasics.farewellUser(username);
+	}
+
+	private static void checkAgainst10() {
+		// TODO Auto-generated method stub
+	}
+
+	private static void checkCurMonth() {
+		System.out.println("Application does a generic activity.");
+		// char letter = AppBasics.requestChar("What is today's Letter?");
+		// System.out.println("Response: " + letter);
+		boolean isHappy = AppBasics.requestBoolean("Are you in a happy mood right now?");
+		System.out.println("Response: " + isHappy);
+		int curSeason = AppBasics.requestInt("What numeric season is it?");
+		switch (curSeason) {
+		case 1:
+			System.out.println("It is Winter.");
+			break;
+		case 2:
+			System.out.println("It is Spring.");
+			break;
+		case 3:
+			System.out.println("It is Summer.");
+			break;
+		case 4:
+			System.out.println("It is Fall.");
+			break;
+		default:
+			System.out.println("Invalid Season.");
+			break;
+		}
+	}
+
+	private static void checkGradeValue() {
 	}
 }
